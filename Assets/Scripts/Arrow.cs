bbb398223041c42ea0e680e593e1f3930ea88403
James <jamesperry27@hotmail.co.uk>
2018-09-23 16:42:13 +0100

    Made first level, and tower shoots arrows

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] Transform thisArrow;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        thisArrow.LookAt(targetEnemy);
+		
+	}
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] Transform tower;
+
+    ParticleSystem arrows;
+
+    private void Start()
+    {
+        arrows = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        tower.LookAt(targetEnemy);
+        
+        
+        
+    }
+}
